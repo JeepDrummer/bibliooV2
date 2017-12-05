0ca2c331278060008f763343a68fcb8232f31e60
Kreolia <fredmouthe@gmail.com>
2017-12-05 12:30:16 +0100

    assets and homepage

---
+++ src/BibliooBundle/Views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Biblioo Cefim</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./../web/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./../web/assets/css/mdb.min.css" rel="stylesheet">
+    <link href="./../web/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light grey lighten-2">
+    <a class="navbar-brand" href="#"><img src="./../web/assets/img/logo.png" class="img-fluid" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <!-- Links -->
+        </ul>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+    </div>
+</nav>
