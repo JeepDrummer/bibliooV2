5f8ab892b7d02383f70a139e06522465e335bd82
jeep <brunet.jph@gmail.com>
2017-12-05 10:51:26 +0100

    Init Biblioo Avec Silex  - MAJ .gitignore

---
+++ src/AdminBundle/Entity/Administrator.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeep
+ * Date: 05/12/2017
+ * Time: 10:20
+ */
+
+namespace AdminBundle\Entity;
+
+
+class Administrator
+{
+    /**
+     * Administrator ID
+     * @var int
+     */
+    private $id;
+
+    /**
+     * Administrator First name
+     * @var string
+     */
+    private $firstname;
+
+    /**
+     * Administrator Last name
+     * @var string
+     */
+    private $lastname;
+
+    /**
+     * Administrator Email
+     * @var string
+     */
+    private $mail;
+
+    /**
+     * Administrator Password
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname(string $firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail(string $mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+}
